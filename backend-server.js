5261af15d2945e5a5fa62825d32d3cba606acab9
marrnig <84583098+marrnig@users.noreply.github.com>
2026-09-13 17:19:25 +0100

    Add title field to availability record creation

--- backend-server.js
+++ backend-server.js
@@ -177,6 +177,7 @@ app.post('/api/availability', async (req, res) => {
       // Create availability record
       const title = `${email} - ${shift.TimeSlot} ${shift.ShiftDate.split('T')[0]}`;
       const fields = {
+        Title: title,
         PersonEmail: email,
         ShiftID: shift.id, // Link to shift
         Available: true,
